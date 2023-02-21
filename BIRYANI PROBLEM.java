b54029c84d2df3ef280977d37aa4df27ea51ddd2
Rxghavdev <mattaraghav67@gmail.com>
2023-02-21 23:58:35 +0530

    FIRST CODE

---
+++ BIRYANI PROBLEM.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{   
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner myInput = new Scanner( System.in );
+	      int  T= myInput.nextInt();
+            for(int i=0;i<T;i++){
+                int X= myInput.nextInt();
+                int Y= myInput.nextInt();
+                System.out.println(X*Y);
+            }
+    }
+}
+	
+
