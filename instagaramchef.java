11f1464d9b728f02584fef434690e1d9ed286f82
Rxghavdev <mattaraghav67@gmail.com>
2023-02-23 23:13:08 +0530

    third code

---
+++ instagaramchef.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner newScanner =new Scanner(System.in);
+		int T=newScanner.nextInt();
+		for(int i=0;i<T;i++){
+		    int X=newScanner.nextInt();
+		    int Y=newScanner.nextInt();
+            //x is following
+            //y is follower
+            int Z= Y*10;
+            if(X>Z){
+                System.out.println("yes");
+                
+            }
+            else{
+            System.out.println("no");
+
+            }
+		}
+	}
+}
